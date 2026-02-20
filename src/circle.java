cc5309221efecd0dba4daa543bbe67e044ab1fec
Yerald26 <yerald.javier.montes@estudiantat.upc.edu>
2026-02-20 20:23:20 +0100

    circulo y triangulo

---
+++ src/circle.java
@@ -0,0 +1,7 @@
+public class circle extends Shape{
+    private double lado;
+
+    public double area (){
+        return pi*lado*lado;
+    }
+}
---
+++ src/triangle.java
@@ -0,0 +1,8 @@
+public class triangle extends Shape {
+    private double b;
+    private double h;
+    public double area (){
+        return (b*h)/2;
+    }
+}
+
