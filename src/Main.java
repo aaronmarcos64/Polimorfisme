27090b4b9d1eb5e955f498dafbdc81ff6890bd8a
aaronmarcos64 <aaron.marcos@estudiantat.upc.edu>
2026-02-27 20:17:28 +0100

    print

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,16 @@ public class Main {
     }
 
     private static void print(Shape[] v) {
+        for (int i=0; i<v.length; i++){
+            System.out.println(v[i].area());;
+        }
     }
 
     private static double suma(Shape[] v) {
+        int u = 0;
+        for (int i=0; i<v.length; i++){
+            u += v[i].area();;
+        }
+        return 0;
     }
 }
